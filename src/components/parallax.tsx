4555318d6ef375f5f8fc0b38fd753be381001f3b
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2024-02-18 00:13:25 +0100

    small changes

--- src/components/parallax.tsx
+++ src/components/parallax.tsx
@@ -1,13 +1,61 @@
-import { motion } from "framer-motion";
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame,
+} from "framer-motion";
+import type { MotionValue } from "framer-motion";
+import { wrap } from "@motionone/utils";
+
 interface ParallaxProps {
   children: string;
   baseVelocity: number;
 }
 
-export default function ParallaxText({ children }: ParallaxProps) {
+export default function ParallaxText({
+  children,
+  baseVelocity = 100,
+}: ParallaxProps) {
+  const baseX: MotionValue<number> = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400,
+  });
+  const velocityFactor: MotionValue<number> = useTransform(
+    smoothVelocity,
+    [0, 1000],
+    [0, 5],
+    {
+      clamp: false,
+    },
+  );
+
+  const x = useTransform(baseX, (v) => `${wrap(-10, -45, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy); // Ensure baseX is MotionValue<number>
+  });
+
   return (
     <div className="parallax">
-      <motion.div>
+      <motion.div className="scroller montserrat font-bold" style={{ x }}>
         <span>{children} </span>
         <span>{children} </span>
         <span>{children} </span>
