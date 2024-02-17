60caf4228a2a34d123f3fd6ed33f78f4b56869f5
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2024-02-18 00:00:15 +0100

    ded

--- src/components/parallax.tsx
+++ src/components/parallax.tsx
@@ -20,7 +20,7 @@ export default function ParallaxText({
   children,
   baseVelocity = 100,
 }: ParallaxProps) {
-  const baseX: MotionValue<any> = useMotionValue(0); // Explicitly typing baseX as MotionValue<number>
+  const baseX: MotionValue<number> = useMotionValue(0); // Explicitly typing baseX as MotionValue<number>
   const { scrollY } = useScroll();
   const scrollVelocity = useVelocity(scrollY);
   const smoothVelocity = useSpring(scrollVelocity, {
