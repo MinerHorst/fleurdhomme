a1ea2723da6809c02a99dbe13db62fea364c65d7
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2024-02-18 13:30:02 +0100

    slight changes

--- src/components/perfumes/spring.tsx
+++ src/components/perfumes/spring.tsx
@@ -12,15 +12,15 @@ export default function SpringComponent() {
       </div>
       <div className="col-span-full row-span-1 border md:col-span-3 md:col-start-3 md:row-span-2 md:row-start-2 md:space-y-10 md:p-2">
         <p className="montserrat hidden w-[75%] flex-col space-y-2 md:flex">
-          The Fleur d'Homme Spring blend breaks free from the ordinary, infusing
-          the classic with the unexpected. Oudh, renowned for its complexity,
-          transforms the familiar into something extraordinary. Picture yourself
-          in a blossoming landscape, surrounded by vibrant scents. Let our
-          fragrance, with its crisp air and rich Oudh undertones, transport you
-          to a new dimension of springtime allure.
+          The Fleur d&apos;Homme Spring blend breaks free from the ordinary,
+          infusing the classic with the unexpected. Oudh, renowned for its
+          complexity, transforms the familiar into something extraordinary.
+          Picture yourself in a blossoming landscape, surrounded by vibrant
+          scents. Let our fragrance, with its crisp air and rich Oudh
+          undertones, transport you to a new dimension of springtime allure.
         </p>
         <div className="montserrat flex flex-col space-y-2 md:hidden">
-          Fleur d'Homme Spring combines the earthy tone of Oudh with the
+          Fleur d&apos;Homme Spring combines the earthy tone of Oudh with the
           freshness of the spring. Embark on a journey to experience fragrances
           like never before.
         </div>
