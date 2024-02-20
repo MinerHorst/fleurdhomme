ebfd8ebf4c331b122366c7d70fa9e0c81e3f8262
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2024-02-20 17:56:37 +0100

    slight changes

--- src/components/perfumes/spring.tsx
+++ src/components/perfumes/spring.tsx
@@ -3,15 +3,15 @@ import ParallaxText from "../parallax";
 import Image from "next/image";
 import spring from "../../../public/spring.png";
 
-export default function SpringComponent() {
+export default function SummerComponent() {
   return (
     <>
-      <section className="flex h-screen w-screen flex-col items-end bg-white py-2 text-black">
-        <div className="z-50 col-span-6 flex h-fit w-full flex-col space-y-2 bg-white shadow-xl shadow-white">
+      <section className="grid h-screen w-screen grid-cols-5 grid-rows-5 bg-white text-black">
+        <div className="space-y-y z-50 col-span-6 flex h-fit w-full flex-col bg-white py-2 shadow-xl shadow-white">
           <ParallaxText baseVelocity={-1}>Fleur d&apos;Homme</ParallaxText>
           <ParallaxText baseVelocity={1}>Spring Spring</ParallaxText>
         </div>
-        <div className="z-50 h-fit w-[35%] bg-white shadow-xl shadow-white md:col-span-2 md:col-start-5">
+        <div className="z-50 col-span-3 col-start-2 row-start-3 h-fit bg-white shadow-xl shadow-white md:col-span-2 md:col-start-2">
           <p className="montserrat z-50 hidden flex-col space-y-2 md:flex">
             The Fleur d&apos;Homme Spring blend breaks free from the ordinary,
             infusing the classic with the unexpected. Oudh, renowned for its
@@ -26,8 +26,12 @@ export default function SpringComponent() {
             fragrances like never before.
           </div>
         </div>
-        <div className="absolute left-0 top-0 z-10">
-          <Image src={spring} alt="drawing of a spring scene." />
+        <div className="absolute left-0 top-0 z-10 hidden md:inline">
+          <Image
+            className="max-w-screen max-h-screen min-h-screen"
+            src={spring}
+            alt="drawing of a summer scene."
+          />
         </div>
       </section>
     </>
--- src/components/perfumes/summer.tsx
+++ src/components/perfumes/summer.tsx
@@ -1,14 +1,39 @@
 import React from "react";
 import ParallaxText from "../parallax";
+import Image from "next/image";
+import summer from "../../../public/summer.png";
 
 export default function SummerComponent() {
   return (
-    <section className="grid h-screen w-screen grid-cols-6 grid-rows-6 bg-[rgb(96,133,169)] text-white">
-      <div className="col-span-6 row-span-1 justify-between border">
-        <ParallaxText baseVelocity={-1}>Fleur d&apos;Homme</ParallaxText>
-        <ParallaxText baseVelocity={1}>Summer Summer</ParallaxText>
-      </div>
-      <div className="col-span-2 row-span-5 row-start-2 border"></div>
-    </section>
+    <>
+      <section className="grid h-screen w-screen grid-cols-5 grid-rows-5 bg-white text-black">
+        <div className="space-y-y z-50 col-span-6 flex h-fit w-full flex-col bg-white py-2 shadow-xl shadow-white">
+          <ParallaxText baseVelocity={1}>Fleur d&apos;Homme</ParallaxText>
+          <ParallaxText baseVelocity={-1}>Summer Summer</ParallaxText>
+        </div>
+        <div className="z-50 col-span-3 col-start-2 row-start-3 h-fit bg-white shadow-xl shadow-white md:col-span-2 md:col-start-2">
+          <p className="montserrat z-50 hidden flex-col space-y-2 md:flex">
+            The Fleur d&apos;Homme Spring blend breaks free from the ordinary,
+            infusing the classic with the unexpected. Oudh, renowned for its
+            complexity, transforms the familiar into something extraordinary.
+            Picture yourself in a blossoming landscape, surrounded by vibrant
+            scents. Let our fragrance, with its crisp air and rich Oudh
+            undertones, transport you to a new dimension of springtime allure.
+          </p>
+          <div className="montserrat flex flex-col space-y-2 md:hidden">
+            Fleur d&apos;Homme Spring combines the earthy tone of Oudh with the
+            freshness of the spring. Embark on a journey to experience
+            fragrances like never before.
+          </div>
+        </div>
+        <div className="absolute left-0 top-0 z-10 hidden md:inline">
+          <Image
+            className="max-w-screen max-h-screen min-h-screen"
+            src={summer}
+            alt="drawing of a summer scene."
+          />
+        </div>
+      </section>
+    </>
   );
 }
