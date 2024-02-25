84a753590d06b4be5bc94c6624a7e58ed7b8c72a
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2024-02-25 14:51:05 +0100

    commit -- making it more mobile responsive

--- src/components/information.tsx
+++ src/components/information.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function InformationComponent() {
   return (
     <>
-      <div className="relative flex h-screen w-screen flex-col items-center justify-evenly text-white">
+      <div className="relative hidden h-screen w-screen flex-col items-center justify-evenly text-white md:flex">
         <div className="montserrat hidden w-[50%] text-center md:inline">
           Fleur d&apos;Homme distinguishes itself by blending fresh florals with
           masculine depth, challenging gender norms in perfumery. Crafted with
--- src/components/perfumes/spring.tsx
+++ src/components/perfumes/spring.tsx
@@ -9,7 +9,7 @@ export default function SummerComponent() {
   return (
     <>
       <section className="relative z-[100] w-screen overflow-x-hidden bg-black text-black">
-        <div className="space-y-y col-span-full row-span-3 row-start-2 flex w-full flex-col justify-center py-2 text-white">
+        <div className="space-y-y col-span-full row-span-3 row-start-2 hidden w-full flex-col justify-center py-2 text-white md:flex">
           <ParallaxText fontsize={250} baseVelocity={-0.5}>
             Fleur d&apos;Homme
           </ParallaxText>
@@ -17,8 +17,58 @@ export default function SummerComponent() {
             Spring Spring
           </ParallaxText>
         </div>
-        <div className="hidden grid-cols-1 place-items-center opacity-80 md:grid md:grid-cols-2 md:gap-[2rem] lg:h-[120vh] lg:grid-cols-4">
-          <div className="col-span-1 flex h-[20vh] w-full flex-col justify-between md:h-[60vh] md:-rotate-[20deg] lg:h-screen">
+        <div className="space-y-y col-span-full row-span-3 row-start-2 flex w-full flex-col justify-center py-2 text-white md:hidden">
+          <ParallaxText fontsize={150} baseVelocity={-0.5}>
+            Fleur d&apos;Homme
+          </ParallaxText>
+          <ParallaxText fontsize={75} baseVelocity={1}>
+            Spring Spring
+          </ParallaxText>
+        </div>
+        <div className="grid  grid-cols-2 place-items-center gap-6 opacity-80 md:gap-[2rem] lg:h-[120vh] lg:grid-cols-4">
+          <div className="col-span-1 flex w-full -rotate-[20deg] flex-col justify-between md:hidden md:h-[60vh] md:-rotate-[20deg] lg:h-screen">
+            <Parallax
+              speed={5}
+              className="flex h-full flex-col justify-between space-y-2"
+            >
+              <div className="h-[50%]">
+                <Image
+                  src={springperfume}
+                  className="h-full w-full"
+                  alt="iamge of a nottel"
+                ></Image>
+              </div>
+              <div className="h-[50%]">
+                <Image
+                  src={springperfume}
+                  className="h-full w-full"
+                  alt="iamge of a nottel"
+                ></Image>
+              </div>
+            </Parallax>
+          </div>
+          <div className="col-span-1 flex w-full -rotate-[20deg] flex-col justify-between md:hidden md:h-[60vh] md:-rotate-[20deg] lg:h-screen">
+            <Parallax
+              speed={-5}
+              className="flex h-full flex-col justify-between space-y-2"
+            >
+              <div className="h-[50%]">
+                <Image
+                  src={springperfume}
+                  className="h-full w-full"
+                  alt="iamge of a nottel"
+                ></Image>
+              </div>
+              <div className="h-[50%]">
+                <Image
+                  src={springperfume}
+                  className="h-full w-full"
+                  alt="iamge of a nottel"
+                ></Image>
+              </div>
+            </Parallax>
+          </div>
+          <div className="ol-span-1 hidden w-full -rotate-[20deg] flex-col justify-between md:flex md:h-[60vh] md:-rotate-[20deg] lg:h-screen">
             <Parallax
               speed={15}
               className="flex h-full flex-col justify-between space-y-2"
@@ -39,9 +89,9 @@ export default function SummerComponent() {
               </div>
             </Parallax>
           </div>
-          <div className="relative col-span-1 h-[20vh] w-full space-y-2 md:h-[60vh] md:-rotate-[20deg] lg:h-screen">
+          <div className="col-span-1 hidden w-full -rotate-[20deg] flex-col justify-between md:h-[60vh] md:-rotate-[20deg] lg:flex lg:h-screen">
             <Parallax
-              speed={0}
+              speed={-5}
               className="relative flex h-full flex-col justify-between space-y-2"
             >
               <div className="h-[50%]">
@@ -61,7 +111,7 @@ export default function SummerComponent() {
               </div>
             </Parallax>
           </div>
-          <div className="col-span-1 h-[20vh] w-full md:h-[60vh] md:-rotate-[20deg] lg:h-screen">
+          <div className="col-span-1 hidden h-[20vh] w-full md:h-[60vh] md:-rotate-[20deg] lg:inline lg:h-screen">
             <Parallax speed={20} className="flex h-full flex-col space-y-2">
               <div className="h-[50%]">
                 <Image
@@ -79,7 +129,7 @@ export default function SummerComponent() {
               </div>
             </Parallax>
           </div>
-          <div className="col-span-1 h-[20vh] w-full md:h-[60vh] md:-rotate-[20deg] lg:h-screen">
+          <div className="col-span-1 hidden h-[20vh] w-full md:inline md:h-[60vh] md:-rotate-[20deg] lg:h-screen">
             <Parallax
               speed={10}
               className="flex h-full flex-col justify-between space-y-2"
@@ -101,12 +151,38 @@ export default function SummerComponent() {
             </Parallax>
           </div>
         </div>
-        <div className="grid h-screen grid-cols-3 bg-black">
-          <div className="col-span-2 col-start-2 flex flex-col justify-center">
+        <div className="flex h-screen flex-col items-center bg-black text-center md:grid md:grid-cols-3 md:text-start">
+          <div className="flex h-full flex-col items-center justify-between space-y-4 md:col-span-2 md:col-start-2 md:justify-center">
             <h1 className="montserrat text-white [font-size:_clamp(3em,4vw,12em)]">
-              Enter a new era of spring perfumes with Fleur d&apos;Homme.
+              <span className="hidden md:inline">
+                Enter a new era of spring perfumes with Fleur d&apos;Homme.
+              </span>
             </h1>
-            <div className="montserrat grid h-[50%] w-full grid-cols-4 grid-rows-2 gap-3">
+            <div className="montserrat flex h-full w-[95%] flex-col justify-between space-y-4 text-start text-white md:hidden">
+              <h1 className="montserrat text-white [font-size:_clamp(3em,4vw,12em)]">
+                <span className="hidden md:inline">
+                  Enter a new era of spring perfumes with Fleur d&apos;Homme.
+                </span>
+                <span className="md:hidden">Enter a new era.</span>
+              </h1>
+              <div className="space-y-4">
+                <p>
+                  The Fleur d&apos;Homme Spring blend breaks free from the
+                  ordinary, infusing the classic with the unexpected. Oudh,
+                  renowned for its complexity, transforms the familiar into
+                  something extraordinary. Picture yourself in a blossoming
+                  landscape, surrounded by vibrant scents. Let our fragrance,
+                  with its crisp air and rich Oudh undertones, transport you to
+                  a new dimension of springtime allure.
+                </p>
+                <button className="z-[56] h-9  bg-white text-black">
+                  Get Fleur d&apos;Homme Spring.
+                </button>
+              </div>
+              <div></div>
+            </div>
+
+            <div className="montserrat hidden h-[50%] w-full grid-cols-4 grid-rows-2 gap-3 md:grid">
               <div className="montserrat col-span-2 row-start-1 text-white">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Asperiores ducimus inventore nam ut, similique quis a provident.
@@ -140,7 +216,7 @@ export default function SummerComponent() {
             </div>
           </div>
         </div>
-        <div className="grid h-screen grid-cols-3 bg-black">
+        <div className="hidden h-screen grid-cols-3 bg-black md:grid">
           <div className="col-span-2 opacity-80">
             <Image src={spring} alt="imag eof a perfume bottle"></Image>
           </div>
@@ -158,7 +234,6 @@ export default function SummerComponent() {
             </button>
           </div>
         </div>
-        <div className="h-[50vh] bg-red-500 md:hidden"></div>
       </section>
     </>
   );
