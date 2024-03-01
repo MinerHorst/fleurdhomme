0f48ecb3c000be25996bf017b30ea993816a020f
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2024-03-02 00:23:02 +0100

    added speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { ReactLenis } from "@studio-freight/react-lenis";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "~/styles/globals.css";
 import { ParallaxProvider } from "react-scroll-parallax";
@@ -8,6 +9,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ParallaxProvider>
       <ReactLenis root>
+        <SpeedInsights />
         <Component {...pageProps} />;{" "}
       </ReactLenis>
     </ParallaxProvider>
